391bfeca82106eb175fc7377e474d72432511159
rossjka <106792994+rossjka@users.noreply.github.com>
2022-06-21 18:37:41 -0500

    autoyes prompts

--- vpnmonitor/install-vpnmonitor.sh
+++ vpnmonitor/install-vpnmonitor.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 echo "please make sure you have already edited 'vpnmonitor' with required input before starting this -- follow the readme"
 echo "installing sqlite3"
-apt-get install sqlite3
+apt-get -y install sqlite3
 echo "installing screen"
-apt-get install screen
+apt-get -y install screen
 echo "creating database 'jkadb.sqlite'"
 sqlite3 jkadb.sqlite "CREATE TABLE iplist (id INTEGER PRIMARY KEY AUTOINCREMENT, ip varchar(30), vpn int, date DATETIME DEFAULT CURRENT_TIMESTAMP);"
 echo "finished creating database 'jkadb.sqlite'"
